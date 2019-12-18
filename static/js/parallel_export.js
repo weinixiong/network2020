1d269a8a6ef1a148be9b688156e4f84dbff0fc30
yehui <1248547721@qq.com>
2019-12-18 23:20:12 +0800

    下拉列表框

--- static/js/parallel_export.js
+++ static/js/parallel_export.js
@@ -2,11 +2,6 @@ function paral() {
     // Schema:
     // 各类商品的import/export值
     var chart = echarts.init(document.getElementById('parallel'));
-    var chart2 = echarts.init(document.getElementById('barchart'));
-
-
-    var dataBalance = Object.values(balance).reverse();
-    var countryList = Object.keys(balance).reverse();
 
     var itemStyle = {
         normal: {
@@ -19,7 +14,6 @@ function paral() {
             shadowColor: 'rgba(0,0,0,0.5)'
         }
     };
-
     //商品种类
     var schema = [
         { name: 'Country', index: 0, text: 'Country' },
@@ -157,14 +151,26 @@ function paral() {
             },
         ]
     };
-
     chart.setOption(option);
+}
 
-    option2 = {
+function barchart(){
+    var chart = echarts.init(document.getElementById('barchart'));
+    var dataBalance = Object.values(balance).reverse();
+    var countryList = Object.keys(balance).reverse();
+
+    option = {
+        title: {
+            text: '进出口差值与GDP比值'
+        },
         tooltip: {
             trigger: "axis",
             axisPointer: {            // 坐标轴指示器，坐标轴触发有效
                 type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            },
+            title: {
+                text: "Balance",
+                show: true
             },
             formatter: function (params, ticket, callback) {
                 var name = params[0].name
@@ -184,7 +190,7 @@ function paral() {
             top: 80,
             bottom: 30
         },
-        xAxis: {
+        yAxis: {
             type: 'value',
             position: 'top',
             splitLine: { lineStyle: { type: 'dashed' } },
@@ -195,7 +201,7 @@ function paral() {
             }
 
         },
-        yAxis: {
+        xAxis: {
             type: 'category',
             axisLine: { show: false },
             axisLabel: { show: false },
@@ -218,11 +224,10 @@ function paral() {
             }
         ]
     };
-    chart2.setOption(option2);
-
-    chart2.on('brushSelected', renderBrushed);
-
+    chart.setOption(option);
+    chart.on('brushSelected', renderBrushed);
 }
+
 function renderBrushed(params) {
     var brushComponent = params.batch[0];
     //这里总共就一个series,貌似不用循环也行，以防万一
@@ -232,49 +237,48 @@ function renderBrushed(params) {
     }
 }
 
-// $(document).ready(function () {
-    //chosen初始化
-    // selcet_ini("#customerId1", ContinentList);
-    // //多选select 数据同步
-    // chose_get_ini("#customerId1");
-    // $("#customerId1").change(function () {
-    //     console.log(chose_get_value("#customerId1"));
-    // });
-// });
+//初始化多选框
+function selcet_ini(select, data) {
+    //添加选项
+    for (let c of data) {
+        $(select).append('<option>' + c.toString() + "</option>")
+    }
+}
 
-// var config = {
-//     '.chosen-select': {},
-//     '.chosen-select-deselect': { allow_single_deselect: true },
-//     '.chosen-select-no-single': { disable_search_threshold: 10 },
-//     '.chosen-select-no-results': { no_results_text: 'Oops, no such country!' },
-//     '.chosen-select-width': { width: "95%" }
-// }
+//select value列表获取
+function chose_get_value(select) {
+    return $(select).val();
+}
 
-// //初始化多选框
-// function selcet_ini(select, data) {
-//     const clist = Object.keys(data)
-//     //添加选项
-//     for (let c of clist) {
-//         $(select).append("<option>" + c.toString() + "</option>")
-//     }
-//     //添加属性值
-//     for (let selector in config) {
-//         $(select).chosen(config[selector]);
-//     }
-// }
+//select text获取，多选时请注意
+function chose_get_text(select) {
+    return $(select + " option:selected").text();
+}
 
-// //select 数据同步
-// function chose_get_ini(select) {
-//     $(select).chosen().change(function () { $(select).trigger("liszt:updated"); });
-// }
+//多选select 数据初始化
+selcet_ini("#countries", COUNTRY);
 
-// //select value列表获取
-// function chose_get_value(select) {
-//     return $(select).val();
-// }
+$('#countries').multipleSelect({
+    addTitle: true, //鼠标点悬停在下拉框时是否显示被选中的值
+    selectAll: true, //是否显示全部复选框，默认显示
+    name: "国家",
+    selectAllText: "选择全部", //选择全部的复选框的text值
+    allSelected: "全部", //全部选中后显示的值
+    delimiter: ', ', //多个值直接的间隔符，默认是逗号
+    placeholder: "选择一个国家", //不选择时下拉框显示的内容
+    keepOpen: true,
+    animate: "slide",
+    filter: true,
+    maxHeightUnit: 'row',
+    maxHeight: 18,
+    isOpen: true
 
-// //select text获取，多选时请注意
-// function chose_get_text(select) {
-//     return $(select + " option:selected").text();
-// }
+});
+
+var selected = [];
+$("#countries").change(function () {
+    selected = chose_get_value("#countries");
+    console.log(selected)
+});
+
 
