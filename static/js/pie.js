a5b03413bcb710fcb640bf31c5772b0588e6508f
weinixiong <669075469@qq.com>
2019-12-19 14:37:04 +0800

    右侧组件添加国家名称

--- static/js/pie.js
+++ static/js/pie.js
@@ -1,97 +1,25 @@
-function pie(L_labels,L_data,r_label,r_data){
-//   option = {
-//     tooltip: {
-//         trigger: 'item',
-//         formatter: "{a} <br/>{b}: {c} ({d}%)"
-//     },
-//     graphic:{
-//         type:'text',
-//         left:'center',
-//         top:'center',
-//         style:{
-//             text:'100',
-//             textAlign:'center',
-//             fill:'#000',
-//             width:30,
-//             height:30
-//         }
-//     },
-//     series: [
-//         {
-//             name,
-//             type:'pie',
-//             radius: ['40%', '55%'],
-//             label: {
-//                 normal: {
-//                     formatter: '{b}：{c}({d}%)',
-//                     // backgroundColor: '#eee',
-//                 }
-//             },
-//             data
-//         }
-//     ]
-// };
-// option = {
-//     tooltip: {
-//         trigger: 'item',
-//         formatter: "{a} <br/>{b}: {c} ({d}%)"
-//     },
-//     legend: {
-//         // orient: 'vertical',
-//         x: 'left',
-//         data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
-//     },
-//     series: [
-//         {
-//             name:'访问来源',
-//             type:'pie',
-//             radius: ['50%', '70%'],
-//             avoidLabelOverlap: false,
-//             label: {
-//                 normal: {
-//                     show: false,
-//                     position: 'center'
-//                 },
-//                 emphasis: {
-//                     show: true,
-//                     textStyle: {
-//                         fontSize: '30',
-//                         fontWeight: 'bold'
-//                     }
-//                 }
-//             },
-//             labelLine: {
-//                 normal: {
-//                     show: false
-//                 }
-//             },
-//             data:[
-//                 {value:335, name:'直接访问'},
-//                 {value:310, name:'邮件营销'},
-//                 {value:234, name:'联盟广告'},
-//                 {value:135, name:'视频广告'},
-//                 {value:1548, name:'搜索引擎'}
-//             ]
-//         }
-//     ]
-// };
-
+function pie(country,L_labels,L_data,r_label,r_data){
+document.getElementById('countryName').innerHTML = country
 option = {
     title: {
-        text: '基础雷达图',
+        text: '出口商品',
         left: 'center',
+        top:'top'
     },
     radar: {
         // shape: 'circle',
         name: {
             textStyle: {
                 color: 'black',
-           }
+                
+           },
+           position:'right'
+           
         },
-        indicator: r_label
+        indicator: r_label,
     },
     series: [{
-        name: '预算 vs 开销（Budget vs spending）',
+        name: '',
         type: 'radar',
         // areaStyle: {normal: {}},
         data : r_data
@@ -107,10 +35,16 @@ option = {
     yAxis: {
         type: 'category',
         axisLabel: {show: false},
-        data: L_labels//['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        data: L_labels,//['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        reverse:true
     },
     xAxis: {
-        type: 'value'
+        type: 'value',
+        axisLabel:{
+            show:false
+        },
+        splitLine:{show:false},
+        position:'top'
     },
     series: [{
         data: L_data,//[120, 200, 150, 80, 70, 110, 130],
@@ -124,8 +58,9 @@ option = {
         label: {
             normal: {
                 show: true,
-                formatter: '{b}',
+                formatter: '{b}({c} 亿)',
                 color:'black',
+                position:"insideLeft"
             }
         },
     }]
